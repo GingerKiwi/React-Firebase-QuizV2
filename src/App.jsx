4e77da645d7c643534023e9e9e0663af5bffba91
Liz GingerKiwi.Dev <liz@gingerkiwi.dev>
2023-08-15 18:00:29 -0400

    fix bug in App.jsx stoping app from rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,23 @@
 import { useState } from 'react'
+
 import './App.css';
 import Home from './components/Home';
 import Game from './components/Game';
 import HighScores from './components/HighScores';
-import { Route, BrowserRouter as Router} from 'react-router-dom';
-
-// import './App.css'
+import { Route, BrowserRouter as Router, Routes} from 'react-router-dom';
 
 function App() {
 
   return (
-
-    <Router> 
-      <div className="container">
-        <Route exact path="/" component={Home} />
-        <Route path="/Game" component={Game} />
-        <Route path="/HighScores" component={HighScores} />
-      </div>
-    </Router>
-
+    <Router> {/* I've also tried without wrapping things in Routes */}
+      <Routes> 
+        {/* <div className="container"> */}
+          <Route exact path="/" element={<Home/>} />
+          <Route path="/Game" component={Game} />
+          <Route path="/HighScores" component={HighScores} />
+        {/* </div> */}
+      </Routes>
+    </Router> 
     
   );
 }
